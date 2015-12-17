05f494b4c3167a8ef4f4c5fb796b328b2f3808ba
BAUD ADRIEN p1303674 <p1303674@univ-lyon1.fr>
2015-12-17 11:23:44 +0100

    First commit. folders and such :v

---
+++ src/corruptiongame/Character/Character.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package corruptiongame.Character;
+
+/**
+ *
+ * @author p1303674
+ */
+public class Character {
+    
+}
---
+++ src/corruptiongame/Character/Stats.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package corruptiongame.Character;
+
+/**
+ *
+ * @author p1303674
+ */
+public enum Stats {
+    STRENGTH,
+    DEFENSE,
+    HEALTH;
+}
